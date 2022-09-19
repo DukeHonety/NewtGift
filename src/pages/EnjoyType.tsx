81093c1e2d37fb6a42d9fc6eea97b6d1899c737d
Duke Honety <mitchellcaleb00@gmail.com>
2022-09-20 01:15:02 +0900

    feat: complete `enjoyTypeForm`

--- src/pages/EnjoyType.tsx
+++ src/pages/EnjoyType.tsx
@@ -1,13 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import bgImg from '../assets/img/mainimg.png';
 import bgTopImg from '../assets/img/righttop.svg';
 import bgBottomImg from '../assets/img/leftbottom.svg';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import StarOnImg from '../assets/img/star_on.png';
 import StarOffImg from '../assets/img/star_off.png';
 
 export const EnjoyType = () => {
+  const [currentLevel, SetLevel] = useState(0);
   const starAry = [0,1,2,3,4];
   return (
     <div className="w-full h-screen max-h-[1060px] overflow-hidden">
@@ -21,8 +22,25 @@ export const EnjoyType = () => {
           <div className="flex flex-col justify-center font-merriweather">
             <p className="text-26 font-bold text-center text-salute">Did you enjoy our product?</p>
           </div>
-          <div className="my-60">
-
+          <div className="my-60 flex justify-center">
+          {
+            starAry.map((level) => {
+              return (
+                <div className="cursor-pointer" onClick={() => SetLevel(level+1)}>
+                  {
+                    level < currentLevel && (
+                      <img src={StarOnImg} className="h-85 w-85"/>
+                    )
+                  }
+                  {
+                    level >= currentLevel && (
+                      <img src={StarOffImg} className="h-85 w-85"/>
+                    )
+                  }
+                </div>
+              );
+            })
+          }
           </div>
           <div className="font-dmsans">
             <button type="button" className="w-full h-70 text-white text-24 font-bold font-sans bg-gradient-to-r from-pinkorange to-pinkocd rounded-12 my-20">NEXT</button>
